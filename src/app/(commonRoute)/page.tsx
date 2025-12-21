ce341915b46c35cb85b6046233caab73e68579fe
arif1101 <arifurrahmanarif223@gmail.com>
2025-12-21 20:52:40 +0600

    feat(ui): hero section

--- src/app/(commonRoute)/page.tsx
+++ src/app/(commonRoute)/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/page/home/hero";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container mx-auto">
+      <Hero/>
     </div>
   );
 }
