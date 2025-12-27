d877b923fba164c4cc9d70fe4d2f9e3c2362d987
arif1101 <arifurrahmanarif223@gmail.com>
2025-12-27 21:25:38 +0600

    feat(ui): course details by single course

--- src/lib/course.ts
+++ src/lib/course.ts
@@ -1,10 +1,24 @@
 export async function getCourses() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/course`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/courses`, {
     cache: "no-store",
   });
 
   if (!res.ok) {
     throw new Error("Failed to fetch courses");
+  }
+
+  return res.json();
+}
+
+// course.ts
+export async function getSingleCourse(id: string) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/courses/${id}`,
+    { cache: "no-store" }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch course");
   }
 
   return res.json();
